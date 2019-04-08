490a29a05adc909299ea9236ef59e689021e479b
League <league@WTS9.attlocal.net>
2019-04-08 14:23:20 -0700

    Nasa Count Down
    Good

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -15,11 +17,19 @@ package extra;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
+		String number = JOptionPane.showInputDialog("chose a number to count down from");
 
 		// 3. Change the countdown to use the new starting point
-		
+		int count = Integer.parseInt(number);
 		// 1. Print a countdown from 10 to 0 on the console
-	
+		for (int i = count; i >= 1; i--) 
+		{			
+			System.out.println(i);
+			Thread.sleep(1000);
+			speak(i + "");
+		}
+		System.out.println("Blast off!");
+		speak("ghjygjydjytdjk!");
 			// 4. Use the speak method to hear the countdown.
 		
 			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
